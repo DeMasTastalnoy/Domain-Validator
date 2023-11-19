709150d36d148696be64c8e71e17574e4f6a683b
FFF5123 <dima.stankov2004@yandex.ru>
2023-11-20 00:36:17 +0300

    Write for PEP 8 standart

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import re
+
+
 def check_domain_name(input_string):
     pattern = re.compile(r'^[a-zA-Z0-9-]{2,63}(?:\.[a-zA-Z0-9-]{2,63}){0,5}$')
     return bool(re.match(pattern, input_string))
+
+
 input_string = "help.stankin.ru"
 print(check_domain_name(input_string))
