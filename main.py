a762eafb2df24ae74cbb8d9fccc1770578eb4756
FFF5123 <dima.stankov2004@yandex.ru>
2023-11-20 16:36:08 +0300

    Make user input capability

--- main.py
+++ main.py
@@ -6,5 +6,19 @@ def check_domain_name(domain):
     return bool(re.match(pattern, domain))
 
 
-input_string = "help.stankin.ru"
-print(check_domain_name(input_string))
+print("Do you want to type in your own domain or check work on default string? [y/n] Type in 'quit' for leave")
+answer = input()
+while answer not in ("quit", "close", "q", "break", "cl", "c"):
+    if answer in ("no", "n", "net"):
+        input_string = "help.stankin.ru"
+        print(input_string)
+        print(check_domain_name(input_string))
+    elif answer in ("yes", "y", "ye", "da"):
+        print("Please type in your domain")
+        input_string = input()
+        print(check_domain_name(input_string))
+    else:
+        print("Answer can be only yes/ye/y/da, no/n/net and quit/close/q/break/cl/c\nTry write the correct variant!")
+    print("Do you want to type in your own domain or check work on default string? [y/n] Type in 'quit' for leave")
+    if answer in ("no", "n", "net", "yes", "y", "ye", "da"):
+        answer = input()
